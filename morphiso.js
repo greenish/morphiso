b3b575d274e9ae2a491be4c97fa85111352f8772
Philipp Adrian <philipp.adrian@dowjones.com>
2015-01-17 13:33:16 +0100

    fixes

--- morphiso.js
+++ morphiso.js
@@ -13,15 +13,17 @@ module.exports.middleware = middleware;
 
 function middleware(filePath){
   var handler = function(req, res, next){
-    filePath = filePath || path.dirname(require.main.filename)+req.originalUrl;
+    filePath = typeof filePath === "string" ? 
+      filePath : 
+      path.dirname(require.main.filename)+req.originalUrl;
 
     fs.createReadStream(filePath)
       .pipe(replaceStream('require('+'"./morphiso-7c8f2a51-d1f5-4534-9a16-1d689c3b3e76"'+');', JSON.stringify(data)))
       .pipe(res);
   }
 
-  if(arguments.length === 1) return handler;
-  else if(arguments.length === 3) handler.apply(this, arguments);
+  if(arguments.length === 3) handler.apply(this, arguments);
+  else return handler;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
