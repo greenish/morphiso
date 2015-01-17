6a2d7d78941dbc5daaf822a2d0b12dee0c84c92c
Philipp Adrian <philipp.adrian@dowjones.com>
2015-01-17 13:27:03 +0100

    allow middleware not to be called in use()

--- morphiso.js
+++ morphiso.js
@@ -12,13 +12,16 @@ module.exports.middleware = middleware;
 ///////////////////////////////////////////////////////////////////////////////
 
 function middleware(filePath){
-  return function(req, res, next){
+  var handler = function(req, res, next){
     filePath = filePath || path.dirname(require.main.filename)+req.originalUrl;
 
     fs.createReadStream(filePath)
       .pipe(replaceStream('require('+'"./morphiso-7c8f2a51-d1f5-4534-9a16-1d689c3b3e76"'+');', JSON.stringify(data)))
       .pipe(res);
   }
+
+  if(arguments.length === 1) return handler;
+  else if(arguments.length === 3) handler.apply(this, arguments);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
