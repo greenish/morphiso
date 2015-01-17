7f4c217c13cac9c4a7c5ffb5116676c88b8be0ba
Philipp Adrian <philipp.adrian@dowjones.com>
2015-01-17 13:09:31 +0100

    basic behavior - done

--- morphiso.js
+++ morphiso.js
@@ -1,24 +1,31 @@
+var replaceStream = require("replacestream");
+var fs = require('fs');
+var path = require("path");
+var data = require("./morphiso-7c8f2a51-d1f5-4534-9a16-1d689c3b3e76");
+
+///////////////////////////////////////////////////////////////////////////////
+
 module.exports = morphiso;
-
-morphiso.middleware = middleware;
+module.exports.middleware = middleware;
 
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-function middleware(req, res, next){
-  next();
+function middleware(filePath){
+  return function(req, res, next){
+    filePath = filePath || path.dirname(require.main.filename)+req.originalUrl;
+
+    fs.createReadStream(filePath)
+      .pipe(replaceStream('require('+'"./morphiso-7c8f2a51-d1f5-4534-9a16-1d689c3b3e76"'+');', JSON.stringify(data)))
+      .pipe(res);
+  }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-function morphiso(key){
-
-  var data = require("./morphiso-7c8f2a51-d1f5-4534-9a16-1d689c3b3e76");
-  
-  data[key] = data[key] || {};
-  
+function morphiso(key, defaults){  
+  data[key] = data[key] || defaults || {};
   return data[key];
-  
 }
 
 ///////////////////////////////////////////////////////////////////////////////
